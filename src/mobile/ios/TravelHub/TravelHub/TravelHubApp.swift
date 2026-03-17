9872b400039ba69437a956620aaca9f05cccc891
afDonosoD <af.donoso@uniandes.edu.co>
2026-03-17 17:53:15 -0500

    Created Swift project

---
+++ src/mobile/ios/TravelHub/TravelHub/TravelHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelHubApp.swift
+//  TravelHub
+//
+//  Created by Andres Donoso on 17/03/26.
+//
+
+import SwiftUI
+
+@main
+struct TravelHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ src/mobile/ios/TravelHub/TravelHubTests/TravelHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelHubTests.swift
+//  TravelHubTests
+//
+//  Created by Andres Donoso on 17/03/26.
+//
+
+import Testing
+@testable import TravelHub
+
+struct TravelHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
