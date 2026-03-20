04a874227318e0c69bb9b9fede50895fd5338b33
Daniel Corzo <d.corzos@uniandes.edu.co>
2026-03-20 14:26:24 -0500

    added language settings

--- src/frontend/src/app/app.config.ts
+++ src/frontend/src/app/app.config.ts
@@ -1,15 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { localeIdFactory } from './core/services/locale.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
+    { provide: LOCALE_ID, useFactory: localeIdFactory },
   ],
 };
